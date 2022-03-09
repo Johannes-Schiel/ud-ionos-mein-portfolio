96cd24c5bd09adf03fd0ad19d23cdd9a9b05e16b
Johannes Schiel <j.schiel@unleashed-design.de>
2022-03-09 15:42:51 +0100

    HeroCardImage

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,44 @@
 <script>
 
+import HeroCard from '@/components/HeroCard.vue';
+
+export default {
+    components: {
+        HeroCard
+    }
+}
+
 </script>
 
 <template>
-
+    <main>
+        <HeroCard />
+    </main>
 </template>
 
-<style>
+<style lang="scss">
+
+@font-face {
+    font-family: $roboto;
+    src: url('@/assets/fonts/Roboto-Regular.ttf');
+    font-weight: 500;
+}
+
+body,
+html {
+    background-color: $second;
+    font-size: 10px;
+    width: 100%;
+	height: 100%;
+    margin: 0;
+	padding: 0;
+    display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+* {
+	box-sizing: border-box;
+}
 
 </style>
