2cda2fea84c5a9f8dee8775a298b96029bc12ad4
Johannes Schiel <j.schiel@unleashed-design.de>
2022-03-09 15:50:02 +0100

    add HeroIntro.vue

--- src/components/HeroCardIntro.vue
+++ src/components/HeroCardIntro.vue
@@ -1,5 +1,9 @@
 <template>
-
+    <article class="intro-text">
+        <p class="intro-hello">Hi, mein Name ist Max Mustermann.</p>
+        <h1>Ich bin ein Front-End Developer<br>& UI/UX Designer.</h1>
+        <p class="content">Aktuell studieren ich in Berlin B.Sc. Informatik und arbeite nebenher an meinem Start-Up zusammen mit einigen Freunden.</p>
+    </article>
 </template>
 
 <script>
@@ -10,5 +14,27 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.intro-text {
+    color: $ciWhite;
+    max-width: 40rem;
+    font-family: $roboto;
+    p {
+        font-size: 1.5rem;
+        padding: 0;
+        margin: 0;
+        &.intro-hello {
+            margin: 2rem 0 1rem 0;
+        }
+        &.content {
+            margin: 0 0 2rem 0;
+            line-height: 1.4em;
+            color: $ciGrey;
+        }
+    }
+    h1 {
+        font-size: 2.5rem;
+        padding: 0;
+        margin: 0 0 1rem 0;
+    }
+}
 </style>
